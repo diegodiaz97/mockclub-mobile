1bc54b2236d2016e3de824c86d91311ec2f89a7d
diegodiaz <dieggodiaz97@gmail.com>
2025-03-19 19:21:08 -0300

    Add blurred image + publication component

---
+++ composeApp/src/androidMain/kotlin/com/diego/futty/design/presentation/preview/BlurImagePreview.kt
@@ -0,0 +1,45 @@
+package com.diego.futty.design.presentation.preview
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.diego.futty.core.presentation.theme.FuttyTheme
+import com.diego.futty.design.presentation.component.image.BlurredImage
+import futty.composeapp.generated.resources.Res
+import futty.composeapp.generated.resources.girasoles
+import org.jetbrains.compose.resources.painterResource
+
+@Preview
+@Composable
+private fun Preview() {
+    FuttyTheme {
+        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            BlurredImage(
+                image = painterResource(Res.drawable.girasoles),
+                blur = 20.dp
+            )
+            BlurredImage(
+                image = painterResource(Res.drawable.girasoles),
+                blur = 30.dp
+            )
+            BlurredImage(
+                image = painterResource(Res.drawable.girasoles),
+                blur = 40.dp
+            )
+            BlurredImage(
+                image = painterResource(Res.drawable.girasoles),
+                blur = 50.dp
+            )
+            BlurredImage(
+                image = painterResource(Res.drawable.girasoles),
+                blur = 60.dp
+            )
+            BlurredImage(
+                image = painterResource(Res.drawable.girasoles),
+                blur = 70.dp
+            )
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/com/diego/futty/profile/presentation/screen/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/com/diego/futty/profile/presentation/screen/ProfileScreen.kt
@@ -14,10 +14,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.diego.futty.core.presentation.theme.colorGrey0
+import com.diego.futty.design.presentation.component.banner.Banner
 import com.diego.futty.design.presentation.component.button.PrimaryButton
+import com.diego.futty.design.presentation.component.image.BlurredImage
 import com.diego.futty.design.presentation.component.topbar.TopBar
 import com.diego.futty.design.presentation.component.topbar.TopBarActionType
 import com.diego.futty.profile.presentation.viewmodel.ProfileViewModel
+import futty.composeapp.generated.resources.Res
+import futty.composeapp.generated.resources.girasoles
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun ProfileScreen(viewModel: ProfileViewModel) {
@@ -50,10 +55,53 @@ private fun ProfileContent(viewModel: ProfileViewModel, paddingValues: PaddingVa
             .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
+        Banner.ClickableBanner(
+            title = "Notificaciones",
+            subtitle = "Éste es un banner que se puede accionar.",
+            onClick = { }
+        ).Draw()
+        Banner.ClickableBanner(
+            title = "Verificar",
+            subtitle = "Éste es un banner que se puede accionar.",
+            onClick = { }
+        ).Draw()
+        Banner.ClickableBanner(
+            title = "Publicidad",
+            subtitle = "Éste es un banner que se puede accionar.",
+            onClick = { }
+        ).Draw()
+        Banner.ClickableBanner(
+            title = "Ayuda",
+            subtitle = "Éste es un banner que se puede accionar.",
+            onClick = { }
+        ).Draw()
+        Banner.ClickableBanner(
+            title = "Guardado",
+            subtitle = "Éste es un banner que se puede accionar.",
+            onClick = { }
+        ).Draw()
+        Banner.ClickableBanner(
+            image = painterResource(Res.drawable.girasoles),
+            title = "Diego Díaz",
+            subtitle = "hace 4hs",
+            onClick = { }
+        ).Draw()
+        BlurredImage(
+            image = painterResource(Res.drawable.girasoles),
+            blur = 50.dp
+        )
+        BlurredImage(
+            image = painterResource(Res.drawable.girasoles),
+            blur = 60.dp
+        )
+        BlurredImage(
+            image = painterResource(Res.drawable.girasoles),
+            blur = 70.dp
+        )
+
         PrimaryButton(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp)
                 .padding(top = 8.dp),
             title = "Cambiar tema",
             isEnabled = true,
